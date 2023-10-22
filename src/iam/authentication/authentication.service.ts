0c977182a9fa1da6028327a57ac3416a7c385302
Haze272 <studiorain27@gmail.com>
2023-10-22 14:17:02 +0300

    feat(auth): add jwt

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,16 +1,22 @@
-import { ConflictException, Injectable, UnauthorizedException } from "@nestjs/common";
+import {ConflictException, Inject, Injectable, UnauthorizedException} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "../../users/entities/user.entity";
 import { Repository } from "typeorm";
 import { HashingService } from "../hashing/hashing.service";
 import { SignUpDto } from "./dto/sign-up.dto/sign-up.dto";
 import { SignInDto } from "./dto/sign-in.dto/sign-in.dto";
+import {JwtService} from "@nestjs/jwt";
+import jwtConfig from "../config/jwt.config";
+import {ConfigType} from "@nestjs/config";
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
-    private readonly hashingService: HashingService
+    private readonly hashingService: HashingService,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY) private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+
   ) {}
 
   async signUp(signUpDto: SignUpDto) {
@@ -44,6 +50,21 @@ export class AuthenticationService {
       throw new UnauthorizedException('Неверный логин/пароль');
     }
 
-    return true;
+    const accessToken = await this.jwtService.signAsync(
+      {
+        sub: user.id,
+        email: user.email,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn: this.jwtConfiguration.accessTokenTtl
+      },
+    );
+
+    return {
+      accessToken
+    };
   }
 }
