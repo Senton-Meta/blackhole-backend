05ded7cf9d95a7605296919366f1e8bda5c1c246
Stepan Berkunov <studiorain27@gmail.com>
2023-10-24 21:47:41 +0300

    feat(authentication): switch auth routes into non-jwt

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,9 +1,12 @@
-import {Body, Controller, HttpCode, HttpStatus, Post, Res} from "@nestjs/common";
-import { Response } from 'express';
-import {AuthenticationService} from "./authentication.service";
-import {SignUpDto} from "./dto/sign-up.dto/sign-up.dto";
-import {SignInDto} from "./dto/sign-in.dto/sign-in.dto";
+import { Body, Controller, HttpCode, HttpStatus, Post, Res } from "@nestjs/common";
+import { Response } from "express";
+import { AuthenticationService } from "./authentication.service";
+import { SignUpDto } from "./dto/sign-up.dto/sign-up.dto";
+import { SignInDto } from "./dto/sign-in.dto/sign-in.dto";
+import { Auth } from "./decorators/auth.decorator";
+import { AuthType } from "./enums/auth-type.enum";
 
+@Auth(AuthType.None)
 @Controller('authentication')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {
