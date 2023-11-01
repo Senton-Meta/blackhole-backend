9d42c2652baaeef0586ded3e26a4c61dd2a21204
Haze272 <studiorain27@gmail.com>
2023-11-01 13:35:48 +0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    { cors: true }
   );
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
